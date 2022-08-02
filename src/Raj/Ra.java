849292e998872579739f126744ed1a481e3935c7
Chintalarajesh <109692986+Chintalarajesh@users.noreply.github.com>
2022-08-02 22:11:23 +0530

    Update Ra.java

--- src/Raj/Ra.java
+++ src/Raj/Ra.java
@@ -4,6 +4,7 @@ public class Ra {
 public static void main(String[] args) {
 	System.out.println("This is Rajesh");
 	System.out.println("I Came from Guntur");
-	System.out.println("I am working in Kanerika");	
+	System.out.println("I am working in Kanerika");
+	System.out.println("Kanerika in Hyderabad");
 }
 }
