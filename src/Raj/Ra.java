9bb69eb0afdac83d9f8f2830da1bc1de9d697eb1
Chintalarajesh <109692986+Chintalarajesh@users.noreply.github.com>
2022-08-02 18:36:30 +0530

    Update Ra.java

--- src/Raj/Ra.java
+++ src/Raj/Ra.java
@@ -3,6 +3,7 @@ package Raj;
 public class Ra {
 public static void main(String[] args) {
 	System.out.println("This is Rajesh");
-	System.out.println("I Came from Guntur")
+	System.out.println("I Came from Guntur");
+	System.out.println("I am working in Kanerika");	
 }
 }
